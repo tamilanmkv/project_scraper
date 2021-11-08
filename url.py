72e65dbe781e9fa0c67214a455dbc7496a18c544
tamilanmkv <tamilanmkv@gmail.com>
2021-11-08 09:14:39 -0800

    merge array and remove dublicates

--- url.py
+++ url.py
@@ -6,27 +6,34 @@ from bs4 import BeautifulSoup
 
 bew = []
 w = []
-#fileter urls 
+#fileter urls with regex
 def url1(bundle,x):
     ex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(ex,bundle)
     k =[k[0] for k in url] 
     return x.append(k)#[i[0] for i in url]
 
+#filter url in `a` href tags
 def spli(bew,lol):
     for link in lol.find_all('a'):
         bew.append(link.get('href'))
     return bew
 
+##add the hipper links into join the domain name
 def withouthttp(soup):
-    print(url1(soup))
+    #print(url1(soup))
     return 0
+
+#map data structure to remove dublicates from array
+def removedub(bew,w):
+    dubs = bew+w
+    return dubs
 
 if __name__ == '__main__':
     boom = requests.get("https://stackoverflow.com/questions/1918270/python-lists-append-return-value").text
     #print(url1(boom))
     soup = BeautifulSoup(boom, 'html.parser')
-   # spli(bew,sou)
+    spli(bew,soup)
    # print(bew) 
-    print(url1(boom,w))
-    print(w)
+    url1(boom,w)
+    print(removedub(bew,w))
