589e55b208c5e537d839abce4b3069371ba1ecf8
tamilanmkv <tamilanmkv@gmail.com>
2021-11-08 08:57:16 -0800

    array flaw change

--- url.py
+++ url.py
@@ -7,22 +7,26 @@ from bs4 import BeautifulSoup
 bew = []
 w = []
 #fileter urls 
-def url1(bundle):
+def url1(bundle,x):
     ex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(ex,bundle)
-    return [x[0] for x in url]
+    k =[k[0] for k in url] 
+    return x.append(k)#[i[0] for i in url]
 
 def spli(bew,lol):
     for link in lol.find_all('a'):
         bew.append(link.get('href'))
     return bew
 
-def withouthttp(bew):
+def withouthttp(soup):
+    print(url1(soup))
     return 0
 
 if __name__ == '__main__':
     boom = requests.get("https://stackoverflow.com/questions/1918270/python-lists-append-return-value").text
     #print(url1(boom))
     soup = BeautifulSoup(boom, 'html.parser')
-    spli(bew,soup)
-    print(bew)    
+   # spli(bew,sou)
+   # print(bew) 
+    print(url1(boom,w))
+    print(w)
