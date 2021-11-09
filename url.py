0c297a86a1629eb45f7f95bea4a7631274e13446
Tom Hudson <mail@tomnomnom.com>
2021-11-09 06:34:40 +0530

    fix dublicates

--- url.py
+++ url.py
@@ -11,7 +11,7 @@ def url1(bundle,x):
     ex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(ex,bundle)
     k =[k[0] for k in url] 
-    return x.append(k)#[i[0] for i in url]
+    return x.append(k)
 
 #filter url in `a` href tags
 def spli(bew,lol):
@@ -26,8 +26,17 @@ def withouthttp(soup):
 
 #map data structure to remove dublicates from array
 def removedub(bew,w):
+    def Convert(a):
+        it = iter(a)
+        res_dct = dict(zip(it, it))
+        return res_dct
     dubs = bew+w
+    dubs = list(dict.fromkeys(Convert(dubs)))
     return dubs
+
+#filter the hipper link and merage the domain url 
+def merge():
+    
 
 if __name__ == '__main__':
     boom = requests.get("https://stackoverflow.com/questions/1918270/python-lists-append-return-value").text
@@ -37,3 +46,6 @@ if __name__ == '__main__':
    # print(bew) 
     url1(boom,w)
     print(removedub(bew,w))
+
+
+    #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
