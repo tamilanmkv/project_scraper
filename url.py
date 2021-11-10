025e27a5567dd86b091ee65c8337695535206686
Tom Hudson <mail@tomnomnom.com>
2021-11-10 11:21:49 +0530

    all basics complete

--- url.py
+++ url.py
@@ -3,11 +3,15 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+import numpy as np
 
-bew = [] #spli functions output
-w = [] #url1 functions output
-x=[]
-nohttps=[]
+split_urls = list() #bew = [] #spli functions output #spli functions output
+regex_urls = list() #w = [] #url1 functions output
+merged_urls=list() #x=[]
+nohttps=list() #
+
+
+
 #fileter urls with regex
 def url1(bundle,w):
     ex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
@@ -23,27 +27,23 @@ def spli(bew,lol):
 
 #map data structure to remove dublicates from array
 def removedub(bew,x,w):
-    dubs = bew+w
-    print(dubs)
-    f = [list(set(k)) for k in dubs]
+    dubs = np.hstack(bew+w)
+    f =list(set(dubs))
     return x.append(f)
 
 #filter the hipper link and merage the domain url 
 def merge(x,nohttps,domain):
-    for i in a:
+    for i in np.hstack(x):
         if str(i).startswith("/"):
             nohttps.append(f"https://{domain}"+i)             
 
 if __name__ == '__main__':
     domain="stackoverflow.com"
     boom = requests.get(f"https://{domain}/").text
-    #print(url1(boom))
     soup = BeautifulSoup(boom, 'html.parser')
-    spli(bew,soup)
-   # print(bew) 
-    url1(boom,w)
-    removedub(bew,x,w)
-    merge(bew,nohttps,domain)
-    print(x)
-    
+    spli(split_urls,soup)
+    url1(boom,regex_urls)
+    removedub(split_urls,merged_urls,regex_urls)
+    merge(merged_urls,nohttps,domain)
+    print(nohttps)
        #litle
